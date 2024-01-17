e09a70439b7eb1765da750bef2ce43dc933d39df
Mirailisc <mirailisclm@gmail.com>
2024-01-18 01:21:44 +0700

    remove unnecessary user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  HttpStatus,
-  UseGuards,
-} from '@nestjs/common'
+import { Controller, Get, Body, Patch, UseGuards, Req } from '@nestjs/common'
 import { UsersService } from './users.service'
-import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
-import { ApiResponse, ApiTags } from '@nestjs/swagger'
+import { ApiTags } from '@nestjs/swagger'
 import { AuthGuard } from '@nestjs/passport'
 
 @ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @ApiResponse({
-    status: HttpStatus.CONFLICT,
-    description: 'Email is already exists',
-  })
-  @ApiResponse({ status: HttpStatus.CREATED, description: 'OK' })
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.usersService.create(createUserDto)
-  }
 
   @UseGuards(AuthGuard('jwt'))
   @Get()
@@ -36,20 +16,8 @@ export class UsersController {
   }
 
   @UseGuards(AuthGuard('jwt'))
-  @Get(':id')
-  async findOneById(@Param('id') id: number) {
-    return await this.usersService.findOneById(id)
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Get(':email')
-  async findOneByEmail(@Param('email') email: string) {
-    return await this.usersService.findOneByEmail(email)
-  }
-
-  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return await this.usersService.update(+id, updateUserDto)
+  async update(@Req() req: any, @Body() updateUserDto: UpdateUserDto) {
+    return await this.usersService.update(req.user.id, updateUserDto)
   }
 }
