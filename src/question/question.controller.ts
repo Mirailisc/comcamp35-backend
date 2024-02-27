9c7e80a6918b5ca5399acfdead62ae96cc5640bb
Mirailisc <mirailisclm@gmail.com>
2024-02-27 20:57:59 +0700

    fix(question): controller

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common'
 import { QuestionService } from './question.service'
 import { AuthGuard } from '@nestjs/passport'
 import { AnswerDto } from './dto/answer.dto'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Question')
 @Controller('question')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
@@ -14,7 +16,7 @@ export class QuestionController {
   }
 
   @UseGuards(AuthGuard('jwt'))
-  @Get(':id')
+  @Get()
   async findOne(@Req() req: any) {
     return await this.questionService.findOne(req.user.id)
   }
