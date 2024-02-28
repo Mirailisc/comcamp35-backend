240eb0bb475c26cbb23c3db63513795a3e573c9f
Mirailisc <mirailisclm@gmail.com>
2024-02-29 00:39:32 +0700

    fix: form

--- src/form/form.service.ts
+++ src/form/form.service.ts
@@ -19,35 +19,48 @@ export class FormService {
       'prefix',
     ])
 
-    const formattedFormDto = omit(formDto, [
-      'first_name',
-      'last_name',
-      'middle_name',
-      'nickname',
-      'prefix',
-    ])
-
     this.logger.log(`User ID ${id} submitted general form`)
 
     try {
-      const formDataByUser = await this.prisma.user.update({
+      await this.prisma.user.update({
         where: { id },
         data: {
           ...userData,
-          Form: {
-            upsert: {
-              create: {
-                ...formattedFormDto,
-                birth_date: new Date(formattedFormDto.birth_date),
-              },
-              update: {
-                ...formattedFormDto,
-                birth_date: new Date(formattedFormDto.birth_date),
-              },
-            },
-          },
         },
-        include: { Form: true },
+      })
+
+      const formDataByUser = await this.prisma.form.upsert({
+        where: { userId: id },
+        update: {
+          tel: formDto.tel,
+          address: formDto.address,
+          travel: formDto.travel,
+          shirt_size: formDto.shirt_size,
+          can_bring_laptop: formDto.can_bring_laptop,
+          food_allergy: formDto.food_allergy,
+          special_food_needs: formDto.special_food_needs,
+          disease: formDto.disease,
+          personal_drug: formDto.personal_drug,
+          drug_allergy: formDto.drug_allergy,
+          insurance: formDto.insurance,
+          birth_date: new Date(formDto.birth_date),
+          userId: id,
+        },
+        create: {
+          tel: formDto.tel,
+          address: formDto.address,
+          travel: formDto.travel,
+          shirt_size: formDto.shirt_size,
+          can_bring_laptop: formDto.can_bring_laptop,
+          food_allergy: formDto.food_allergy,
+          special_food_needs: formDto.special_food_needs,
+          disease: formDto.disease,
+          personal_drug: formDto.personal_drug,
+          drug_allergy: formDto.drug_allergy,
+          insurance: formDto.insurance,
+          birth_date: new Date(formDto.birth_date),
+          userId: id,
+        },
       })
 
       return await this.findOne(formDataByUser.id)
@@ -58,7 +71,7 @@ export class FormService {
 
   async findOne(id: number): Promise<Form> {
     return await this.prisma.form.findUnique({
-      where: { id },
+      where: { userId: id },
     })
   }
 }
