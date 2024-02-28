ec7b73f8af996ca142570e88eb296afd1a838046
Mirailisc <mirailisclm@gmail.com>
2024-02-29 02:12:25 +0700

    fix: a lot :(

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,6 +3,7 @@ import { File as PrismaFile } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UploadFileDto } from './dto/upload-file.dto'
 
+const MIN_FILE = 5
 @Injectable()
 export class FileService {
   private logger: Logger = new Logger('S3')
@@ -10,17 +11,37 @@ export class FileService {
   constructor(private prisma: PrismaService) {}
 
   async uploadFile(id: number, uploadFileDto: UploadFileDto) {
-    const res = await this.prisma.file.upsert({
-      where: { userId: id },
-      create: { ...uploadFileDto, userId: id },
-      update: {
-        ...uploadFileDto,
-        userId: id,
-      },
-    })
+    const existFile = await this.getFileUrlByUserId(id)
 
-    this.logger.log(`User ${id} uploaded file to ${res.url}`)
-    return res.url
+    if (
+      uploadFileDto.type === 'academic-answer' ||
+      existFile.length <= MIN_FILE
+    ) {
+      const res = await this.prisma.file.create({
+        data: {
+          ...uploadFileDto,
+          userId: id,
+        },
+      })
+
+      this.logger.log(`User ${id} uploaded file to ${res.url}`)
+      return res.url
+    } else {
+      const existFile = await this.prisma.file.findFirst({
+        where: { userId: id, type: uploadFileDto.type },
+      })
+
+      const res = await this.prisma.file.update({
+        where: { id: existFile.id, userId: id, type: uploadFileDto.type },
+        data: {
+          ...uploadFileDto,
+          userId: id,
+        },
+      })
+
+      this.logger.log(`User ${id} updated ${res.type} file`)
+      return res.url
+    }
   }
 
   async getFileUrlByUserId(id: number): Promise<PrismaFile[]> {
