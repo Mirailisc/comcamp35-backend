37edca2bb4d65a0a49cc8f299c268c5fc99c8899
Mirailisc <mirailisclm@gmail.com>
2024-02-29 11:19:06 +0700

    fix: file upload

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,9 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common'
-import { File as PrismaFile } from '@prisma/client'
+import { File, File as PrismaFile } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UploadFileDto } from './dto/upload-file.dto'
 
-const MIN_FILE = 5
 @Injectable()
 export class FileService {
   private logger: Logger = new Logger('S3')
@@ -13,10 +12,28 @@ export class FileService {
   async uploadFile(id: number, uploadFileDto: UploadFileDto) {
     const existFile = await this.getFileUrlByUserId(id)
 
-    if (
-      uploadFileDto.type === 'academic-answer' ||
-      existFile.length <= MIN_FILE
-    ) {
+    // get exist file based on uploadFileDto.type
+    // if not exist, create new file
+    // if exist, update file
+
+    this.logger.log(`User ${id} uploading file`)
+
+    const getFileByType = existFile.find(
+      (file: File) => file.type === uploadFileDto.type,
+    )
+
+    if (getFileByType) {
+      const res = await this.prisma.file.update({
+        where: { id: getFileByType.id, userId: id, type: uploadFileDto.type },
+        data: {
+          ...uploadFileDto,
+          userId: id,
+        },
+      })
+
+      this.logger.log(`User ${id} updated ${res.type} file`)
+      return res.url
+    } else {
       const res = await this.prisma.file.create({
         data: {
           ...uploadFileDto,
@@ -25,21 +42,6 @@ export class FileService {
       })
 
       this.logger.log(`User ${id} uploaded file to ${res.url}`)
-      return res.url
-    } else {
-      const existFile = await this.prisma.file.findFirst({
-        where: { userId: id, type: uploadFileDto.type },
-      })
-
-      const res = await this.prisma.file.update({
-        where: { id: existFile.id, userId: id, type: uploadFileDto.type },
-        data: {
-          ...uploadFileDto,
-          userId: id,
-        },
-      })
-
-      this.logger.log(`User ${id} updated ${res.type} file`)
       return res.url
     }
   }
